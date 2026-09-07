a510079af61949406c57aa97cba74f205f114e0a
Hasan Zaidi <hasan.zaidi1248@gmail.com>
2026-09-06 21:35:33 -0600

    Fix React hook lint warning in HeroSection

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -138,6 +138,7 @@ export function HeroSection() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
